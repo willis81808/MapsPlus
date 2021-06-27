2fd8e11fe2ff2944ced9d2abcd781732cb74f124
willis81808 <willis81808@gmail.com>
2021-06-26 18:15:30 -0600

    Added two new maps

--- MapsPlus.cs
+++ MapsPlus.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 namespace MapsPlusPlugin
 {
     [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin(ModId, ModName, "0.0.1")]
+    [BepInPlugin(ModId, ModName, "0.0.2")]
     [BepInProcess("Rounds.exe")]
     public class MapsPlus : BaseUnityPlugin
     {
